317506ca62a55471583544957708757b240bb4b6
asyrawih <hasyrawi@gmail.com>
2023-07-13 15:53:23 +0800

    WIP: add rate start container

--- app/detail/[slug]/page.tsx
+++ app/detail/[slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Image } from "@nextui-org/image";
-import { Card, CardBody, CardHeader, Chip, Divider, Spacer } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Checkbox, Chip, Divider, Spacer } from "@nextui-org/react";
 
 
 export default function DetailPages({ params }: { params: { slug: string } }) {
@@ -21,17 +21,19 @@ export default function DetailPages({ params }: { params: { slug: string } }) {
         />
         {/* Desktop Version */}
         <div className="hidden lg:flex">
-          <div className="relative h-40 w-40 left-5 z-20 -top-[100px]">
-            <Image
-              src='https://komikcast.io/wp-content/uploads/2020/04/jhfdjksahkj289a-e1586171017310.jpg'
-              radius='none'
-              loading='lazy'
-              className="w-full object-center"
-              removeWrapper
-            />
+          <div className="w-1/3">
+            <div className="relative h-40 w-40 left-5 z-20 -top-[100px]">
+              <Image
+                src='https://komikcast.io/wp-content/uploads/2020/04/jhfdjksahkj289a-e1586171017310.jpg'
+                radius='none'
+                loading='lazy'
+                className="w-full object-center"
+                removeWrapper
+              />
+            </div>
           </div>
           <Spacer x={12} />
-          <div className="z-30 mt-2">
+          <div className="z-30 mt-2 flex flex-col justify-between w-full">
             <div>
               <span className="text-4xl font-bold text-gray-500">Kuro no Shoukanshi Bahasa Indonesia</span>
             </div>
@@ -44,6 +46,27 @@ export default function DetailPages({ params }: { params: { slug: string } }) {
               <Chip className="mx-1">Isekai</Chip>
               <Chip className="mx-1">Doujin</Chip>
               <Chip className="mx-1">Hentai</Chip>
+            </div>
+            <div className="flex flex-row gap-1 mt-2 mb-2 justify-between">
+              <div className="flex flex-col">
+                <Checkbox defaultSelected size="sm" >Released : {'2022'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Status: {'ongoing'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Total Chapter {'200'}</Checkbox>
+              </div>
+              <div className="flex flex-col">
+                <Checkbox defaultSelected size="sm" >Author: {'hanan'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Type : {'manhwa'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Update On: {'july 2022'}</Checkbox>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full justify-around mt-2">
+            <div className="flex flex-col">
+              <div className="w-40 h-40 bg-gray-500">
+                Rating Start In Here
+              </div>
+              <Spacer y={3} />
+              <Button>Bookmark</Button>
             </div>
           </div>
         </div>
@@ -63,6 +86,19 @@ export default function DetailPages({ params }: { params: { slug: string } }) {
               <Chip className="mx-1 my-1">Isekai</Chip>
               <Chip className="mx-1 my-1">Doujin</Chip>
               <Chip className="mx-1 my-1">Hentai</Chip>
+            </div>
+
+            <div className="flex flex-row gap-1 mt-2 mb-2 justify-between">
+              <div className="flex flex-col">
+                <Checkbox defaultSelected size="sm" >Released : {'2022'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Status: {'ongoing'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Total Chapter {'200'}</Checkbox>
+              </div>
+              <div className="flex flex-col">
+                <Checkbox defaultSelected size="sm" >Author: {'hanan'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Type : {'manhwa'}</Checkbox>
+                <Checkbox defaultSelected size="sm" >Update On: {'july 2022'}</Checkbox>
+              </div>
             </div>
           </div>
         </div>
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -11,6 +11,7 @@ export default function Navigation() {
     "Populer",
     "Projects",
     "Bookmarks",
+    "Music",
     "Log Out",
   ];
 
