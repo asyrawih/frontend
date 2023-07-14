6082c7414cfd58e4d9d0c499d893142d6c4efbdd
asyrawih <hasyrawi@gmail.com>
2023-07-14 22:08:46 +0800

    WIP: tweaks responsive on grid on root pages

--- components/pages/home/latest.tsx
+++ components/pages/home/latest.tsx
@@ -15,7 +15,7 @@ export const LatestSection = ({ title }: LatestSectionProps) => {
         </CardHeader>
         <Divider />
         <CardBody className='p-0 mt-3'>
-          <div className='grid gap-2 grid-cols-1 sm:grid-cols-3'>
+          <div className='grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
             <MangaBoxContainer />
             <MangaBoxContainer />
             <MangaBoxContainer />
@@ -57,17 +57,17 @@ const MangaBoxContainer = () => {
       <div className='flex flex-col w-full'>
         <span className='font-bold text-white ml-2 mb-2'>Some Text</span>
         <div id='chapter_continer' className='flex justify-between mt-1'>
-          <Chip variant='faded' color='default' className='ml-2 cursor-pointer hover:text-blue-300'>Ch.1</Chip>
+          <Chip variant='faded' size='sm' color='default' className='ml-2 cursor-pointer hover:text-blue-300'>Ch.1</Chip>
           <span className='ml-2 text-xs text-gray-600'>2 hours ago</span>
         </div>
 
         <div id='chapter_continer' className='flex justify-between mt-1'>
-          <Chip variant='faded' color='default' className='ml-2 cursor-pointer hover:text-blue-300'>Ch.1</Chip>
+          <Chip variant='faded' color='default' size='sm' className='ml-2 cursor-pointer hover:text-blue-300'>Ch.1</Chip>
           <span className='ml-2 text-xs text-gray-600'>2 hours ago</span>
         </div>
 
         <div id='chapter_continer' className='flex justify-between mt-1'>
-          <Chip variant='faded' color='default' className='ml-2 cursor-pointer hover:text-blue-300'>Ch.1</Chip>
+          <Chip variant='faded' color='default' size='sm' className='ml-2 cursor-pointer hover:text-blue-300'>Ch.1</Chip>
           <span className='ml-2 text-xs text-gray-600'>2 hours ago</span>
         </div>
 
